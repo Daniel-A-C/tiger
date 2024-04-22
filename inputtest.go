b8125b9ee78324818580f39fb46a3ab5ba7116e7
Daniel Carroll <danalexanderc@gmail.com>
2024-04-22 13:39:52 -0400

    working inputtest

---
+++ inputtest.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/gdamore/tcell/v2"
+)
+
+func main() {
+
+	s, e := tcell.NewScreen()
+	//w, h := s.Size()
+	if e != nil {
+		fmt.Fprintf(os.Stderr, "%v\n", e)
+		os.Exit(1)
+	}
+	if e := s.Init(); e != nil {
+		fmt.Fprintf(os.Stderr, "%v\n", e)
+		os.Exit(1)
+	}
+
+	fmt.Fprintf(os.Stdout, "hello")
+	for {
+		switch ev := s.PollEvent().(type) {
+		case *tcell.EventKey:
+			fmt.Print(" ", string(ev.Rune()))
+			if ev.Key() == tcell.KeyEscape {
+				s.Fini()
+				os.Exit(0)
+			}
+		}
+	}
+}
